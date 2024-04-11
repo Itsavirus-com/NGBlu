3c2808a78cbe1a223fec2502356ac6a5b7a3f00e
Mega Suartika <mega@itsavirus.com>
2024-04-12 00:03:23 +0800

    feat: migrate metronic template & add auth pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+import '@/assets/sass/style.scss'
+
+const inter = Inter({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "InfraOrders 2.0",
-};
+  title: 'InfraOrders 2.0',
+  description: 'Fast, Efficient and Productive',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-bs-theme="light">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
