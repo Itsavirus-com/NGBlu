c9706623e171b278c891e8bb53bfd8cabd74f806
Mega Suartika <mega@itsavirus.com>
2024-08-08 16:48:18 +0800

    refactor: move address details to a separate component

--- src/app/[locale]/dashboard/data-validation/validation/components/business-partner-content.tsx
+++ src/app/[locale]/dashboard/data-validation/validation/components/business-partner-content.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 
+import { AddressDetails } from './address-details'
 import { ContentProps } from './dynamic-drawer.type'
 import { TextWithLabel } from './text-with-label'
 
@@ -26,155 +27,25 @@ export const BusinessPartnerContent = ({ data }: ContentProps) => {
 
       {data.companyInfo?.legalAddress && (
         <Tab eventKey="legalAddress" title={t('address.legalAddress')}>
-          <TextWithLabel
-            className="mt-5"
-            label={t('address.addressName')}
-            value={data.companyInfo.legalAddress.addressName}
-          />
-          <TextWithLabel
-            label={t('address.streetName')}
-            value={data.companyInfo.legalAddress.streetname}
-          />
-          <TextWithLabel
-            label={t('address.houseNumber')}
-            value={data.companyInfo.legalAddress.housenumber}
-          />
-          <TextWithLabel
-            label={t('address.houseNumberSuffix')}
-            value={data.companyInfo.legalAddress.housenumberSuffix}
-          />
-          <TextWithLabel
-            label={t('address.apartmentNumber')}
-            value={data.companyInfo.legalAddress.appartmentNumber}
-          />
-          <TextWithLabel label={t('address.area')} value={data.companyInfo.legalAddress.area} />
-          <TextWithLabel label={t('address.county')} value={data.companyInfo.legalAddress.county} />
-          <TextWithLabel label={t('address.city')} value={data.companyInfo.legalAddress.city} />
-          <TextWithLabel
-            label={t('address.country')}
-            value={data.companyInfo.legalAddress.country.name}
-          />
-          <TextWithLabel
-            label={t('address.postalCode')}
-            value={data.companyInfo.legalAddress.postalcode}
-          />
+          <AddressDetails address={data.companyInfo.legalAddress} />
         </Tab>
       )}
 
       {data.companyInfo?.postalAddress && (
-        <Tab eventKey="profile" title={t('address.postalAddress')}>
-          <TextWithLabel
-            className="mt-5"
-            label={t('address.addressName')}
-            value={data.companyInfo.postalAddress.addressName}
-          />
-          <TextWithLabel
-            label={t('address.streetName')}
-            value={data.companyInfo.postalAddress.streetname}
-          />
-          <TextWithLabel
-            label={t('address.houseNumber')}
-            value={data.companyInfo.postalAddress.housenumber}
-          />
-          <TextWithLabel
-            label={t('address.houseNumberSuffix')}
-            value={data.companyInfo.postalAddress.housenumberSuffix}
-          />
-          <TextWithLabel
-            label={t('address.apartmentNumber')}
-            value={data.companyInfo.postalAddress.appartmentNumber}
-          />
-          <TextWithLabel label={t('address.area')} value={data.companyInfo.postalAddress.area} />
-          <TextWithLabel
-            label={t('address.county')}
-            value={data.companyInfo.postalAddress.county}
-          />
-          <TextWithLabel label={t('address.city')} value={data.companyInfo.postalAddress.city} />
-          <TextWithLabel
-            label={t('address.country')}
-            value={data.companyInfo.postalAddress.country.name}
-          />
-          <TextWithLabel
-            label={t('address.postalCode')}
-            value={data.companyInfo.postalAddress.postalcode}
-          />
+        <Tab eventKey="postalAddress" title={t('address.postalAddress')}>
+          <AddressDetails address={data.companyInfo.postalAddress} />
         </Tab>
       )}
 
       {data.companyInfo?.visitAddress && (
         <Tab eventKey="visitAddress" title={t('address.visitAddress')}>
-          <TextWithLabel
-            className="mt-5"
-            label={t('address.addressName')}
-            value={data.companyInfo.visitAddress.addressName}
-          />
-          <TextWithLabel
-            label={t('address.streetName')}
-            value={data.companyInfo.visitAddress.streetname}
-          />
-          <TextWithLabel
-            label={t('address.houseNumber')}
-            value={data.companyInfo.visitAddress.housenumber}
-          />
-          <TextWithLabel
-            label={t('address.houseNumberSuffix')}
-            value={data.companyInfo.visitAddress.housenumberSuffix}
-          />
-          <TextWithLabel
-            label={t('address.apartmentNumber')}
-            value={data.companyInfo.visitAddress.appartmentNumber}
-          />
-          <TextWithLabel label={t('address.area')} value={data.companyInfo.visitAddress.area} />
-          <TextWithLabel label={t('address.county')} value={data.companyInfo.visitAddress.county} />
-          <TextWithLabel label={t('address.city')} value={data.companyInfo.visitAddress.city} />
-          <TextWithLabel
-            label={t('address.country')}
-            value={data.companyInfo.visitAddress.country.name}
-          />
-          <TextWithLabel
-            label={t('address.postalCode')}
-            value={data.companyInfo.visitAddress.postalcode}
-          />
+          <AddressDetails address={data.companyInfo.visitAddress} />
         </Tab>
       )}
 
       {data.companyInfo?.invoiceAddress && (
         <Tab eventKey="invoiceAddress" title={t('address.invoiceAddress')}>
-          <TextWithLabel
-            className="mt-5"
-            label={t('address.addressName')}
-            value={data.companyInfo.invoiceAddress.addressName}
-          />
-          <TextWithLabel
-            label={t('address.streetName')}
-            value={data.companyInfo.invoiceAddress.streetname}
-          />
-          <TextWithLabel
-            label={t('address.houseNumber')}
-            value={data.companyInfo.invoiceAddress.housenumber}
-          />
-          <TextWithLabel
-            label={t('address.houseNumberSuffix')}
-            value={data.companyInfo.invoiceAddress.housenumberSuffix}
-          />
-          <TextWithLabel
-            label={t('address.apartmentNumber')}
-            value={data.companyInfo.invoiceAddress.appartmentNumber}
-          />
-          <TextWithLabel label={t('address.area')} value={data.companyInfo.invoiceAddress.area} />
-          <TextWithLabel
-            label={t('address.county')}
-            value={data.companyInfo.invoiceAddress.county}
-          />
-          <TextWithLabel label={t('address.city')} value={data.companyInfo.invoiceAddress.city} />
-          <TextWithLabel
-            label={t('address.country')}
-            value={data.companyInfo.invoiceAddress.country.name}
-          />
-          <TextWithLabel
-            label={t('address.postalCode')}
-            value={data.companyInfo.invoiceAddress.postalcode}
-          />
+          <AddressDetails address={data.companyInfo.invoiceAddress} />
         </Tab>
       )}
     </Tabs>
--- src/app/[locale]/dashboard/data-validation/validation/components/enterprise-root-content.tsx
+++ src/app/[locale]/dashboard/data-validation/validation/components/enterprise-root-content.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 
+import { AddressDetails } from './address-details'
 import { ContentProps } from './dynamic-drawer.type'
 import { TextWithLabel } from './text-with-label'
 
@@ -16,50 +17,9 @@ export const EnterpriseRootContent = ({ data }: ContentProps) => {
 
       {data.enterpriseRootAddresses && (
         <Tab eventKey="address" title={t('address.address')}>
-          <TextWithLabel
-            className="mt-5"
-            label={t('address.addressName')}
-            value={data.enterpriseRootAddresses.address.addressName}
-          />
-          <TextWithLabel
-            label={t('address.addressType')}
-            value={data.enterpriseRootAddresses.addressType.addressType}
-          />
-          <TextWithLabel
-            label={t('address.streetName')}
-            value={data.enterpriseRootAddresses.address.streetname}
-          />
-          <TextWithLabel
-            label={t('address.houseNumber')}
-            value={data.enterpriseRootAddresses.address.housenumber}
-          />
-          <TextWithLabel
-            label={t('address.houseNumberSuffix')}
-            value={data.enterpriseRootAddresses.address.housenumberSuffix}
-          />
-          <TextWithLabel
-            label={t('address.apartmentNumber')}
-            value={data.enterpriseRootAddresses.address.appartmentNumber}
-          />
-          <TextWithLabel
-            label={t('address.area')}
-            value={data.enterpriseRootAddresses.address.area}
-          />
-          <TextWithLabel
-            label={t('address.county')}
-            value={data.enterpriseRootAddresses.address.county}
-          />
-          <TextWithLabel
-            label={t('address.city')}
-            value={data.enterpriseRootAddresses.address.city}
-          />
-          <TextWithLabel
-            label={t('address.country')}
-            value={data.enterpriseRootAddresses.address.country.name}
-          />
-          <TextWithLabel
-            label={t('address.postalCode')}
-            value={data.enterpriseRootAddresses.address.postalcode}
+          <AddressDetails
+            address={data.enterpriseRootAddresses.address}
+            addressType={data.enterpriseRootAddresses.addressType}
           />
         </Tab>
       )}
