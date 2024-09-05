58e1a3414d91bbccb012f72ee5c95210f963c95a
Mega Suartika <mega@itsavirus.com>
2024-09-05 12:03:33 +0800

    add: date time view component

---
+++ src/components/view/date-time-view/date-time-view.tsx
@@ -0,0 +1,26 @@
+import dayjs from 'dayjs'
+import { Col, Placeholder } from 'react-bootstrap'
+
+import { DateTimeViewProps, dateTimeFormats } from './date-time-view.type'
+
+export const DateTimeView = (props: DateTimeViewProps) => {
+  const { label, value, disableColumn, isLoading, format = 'default', ...colProps } = props
+
+  const formattedValue = value ? dayjs(value).format(dateTimeFormats[format]) : '-'
+
+  const renderContent = () => (
+    <>
+      {label && <label className="fw-bold form-label mb-1">{label}</label>}
+      {isLoading && <Placeholder size="lg" bg="gray-500" className="w-100" />}
+      {!isLoading && <p className="m-0">{formattedValue}</p>}
+    </>
+  )
+
+  if (disableColumn) return renderContent()
+
+  return (
+    <Col xs={12} md={6} lg={4} {...colProps}>
+      {renderContent()}
+    </Col>
+  )
+}
