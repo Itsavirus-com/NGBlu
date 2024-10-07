83220dbb30d3551d1fc799b76b49bacd212197f0
Mega Suartika <mega@itsavirus.com>
2024-10-07 18:41:21 +0800

    add: role and onClick props on icon component

--- src/components/kt-icon/kt-icon.tsx
+++ src/components/kt-icon/kt-icon.tsx
@@ -6,11 +6,17 @@ type Props = {
   className?: string
   iconType?: 'duotone' | 'solid' | 'outline'
   iconName: string
+  role?: string
+  onClick?: () => void
 }
 
-const KTIcon: FC<Props> = ({ className = '', iconType = 'duotone', iconName }) => {
+const KTIcon: FC<Props> = ({ className = '', iconType = 'duotone', iconName, role, onClick }) => {
   return (
-    <i className={`ki-${iconType} ki-${iconName}${className && ' ' + className}`}>
+    <i
+      className={`ki-${iconType} ki-${iconName}${className && ' ' + className}`}
+      role={role}
+      onClick={onClick}
+    >
       {iconType === 'duotone' &&
         [...Array(icons[iconName])].map((_e, i) => {
           return (
