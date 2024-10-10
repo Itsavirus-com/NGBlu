8fd7dfda0fc62cf86892abf7a120ee314194bfaf
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-10-10 16:40:37 +0700

    feat(product-type-crud): Create and Implement CRUD of Product Type API

---
+++ src/app/[locale]/dashboard/data-management/product/types/[typeId]/edit/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import { useTranslations } from 'next-intl'
+import { Card, CardBody } from 'react-bootstrap'
+
+import { FormButtons } from '@/components/forms/form-buttons'
+import { FormProvider } from '@/components/forms/form-provider'
+import { ControlledInput } from '@/components/forms/input'
+import { PageTitle } from '@/components/page-title'
+
+import useProductTypeForm from '../../components/product-type-form.hook'
+
+export default function UpdateProductType() {
+  const { typeId } = useParams()
+  const t = useTranslations('dataManagement.products.types')
+
+  const { methods, onSubmit } = useProductTypeForm(Number(typeId))
+
+  return (
+    <>
+      <PageTitle title={t('updateProductType')} />
+
+      <FormProvider methods={methods} onSubmit={onSubmit}>
+        <div className="app-container container-fluid">
+          <Card>
+            <CardBody>
+              <ControlledInput
+                label={t('type')}
+                name="productType"
+                containerClass="mb-3"
+                className="form-control-solid"
+              />
+
+              <FormButtons />
+            </CardBody>
+          </Card>
+        </div>
+      </FormProvider>
+    </>
+  )
+}
