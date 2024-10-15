407fd4eb1feb51d0734034e16a09b3ee619fa3c1
Victor Angelier <victor@ngblunetworks.nl>
2024-10-15 12:19:45 +0200

    Fix for image problems

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+      domains: [
+        'localhost',
+        'io2-frontend.development.ngblu.io',
+        'io2-frontend.staging.ngblu.io'
+      ],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      formats: ['image/webp'], // Optioneel als je WebP wilt ondersteunen
 }
 
 export default withNextIntl(nextConfig)
