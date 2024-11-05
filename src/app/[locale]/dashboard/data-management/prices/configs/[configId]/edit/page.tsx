b89b78fc2da9b9e932e2121d339a590e5926bb71
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-11-05 08:07:48 +0700

    feat(price-config-crud): Create and implement the CRUD of price config

---
+++ src/app/[locale]/dashboard/data-management/prices/configs/[configId]/edit/page.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { useTranslations } from 'next-intl'
+import { Card, CardBody } from 'react-bootstrap'
+
+import { FormButtons } from '@/components/forms/form-buttons'
+import { FormProvider } from '@/components/forms/form-provider'
+import { ControlledInput } from '@/components/forms/input'
+import { ControlledSelect } from '@/components/forms/select'
+import { PageTitle } from '@/components/page-title'
+import { PriceCurrency } from '@/services/swr/models/price-currency.type'
+import { PriceInterval } from '@/services/swr/models/price-interval.type'
+import { PriceTax } from '@/services/swr/models/price-tax.type'
+import { PriceType } from '@/services/swr/models/price-type.type'
+import { PriceUnit } from '@/services/swr/models/price-unit.type'
+
+import usePriceConfigForm from '../../components/price-config-form.hook'
+
+export default function UpdatePriceConfig({ params }: { params: { configId: number } }) {
+  const t = useTranslations('dataManagement.prices.configs')
+
+  const { methods, onSubmit } = usePriceConfigForm(Number(params.configId))
+
+  return (
+    <>
+      <PageTitle title={t('updatePriceConfig')} />
+
+      <FormProvider methods={methods} onSubmit={onSubmit}>
+        <div className="app-container container-fluid">
+          <Card>
+            <CardBody>
+              <ControlledInput
+                label={t('price')}
+                name="priceValue"
+                containerClass="mb-3"
+                className="form-control-solid"
+              />
+              <ControlledSelect<PriceUnit>
+                label={t('unit')}
+                name="priceUnitId"
+                containerClass="mb-3"
+                className="form-select"
+                apiPath="prices/units"
+                option={{ label: row => row.unit, value: row => row.id }}
+              />
+              <ControlledSelect<PriceCurrency>
+                label={t('currency')}
+                name="priceCurrencyId"
+                containerClass="mb-3"
+                className="form-select"
+                apiPath="prices/currencies"
+                option={{ label: row => row.currency, value: row => row.id }}
+              />
+              <ControlledSelect<PriceType>
+                label={t('type')}
+                name="priceTypeId"
+                containerClass="mb-3"
+                className="form-select"
+                apiPath="prices/types"
+                option={{ label: row => row.type, value: row => row.id }}
+              />
+              <ControlledSelect<PriceInterval>
+                label={t('interval')}
+                name="priceIntervalId"
+                containerClass="mb-3"
+                className="form-select"
+                apiPath="prices/intervals"
+                option={{ label: row => row.name, value: row => row.id }}
+              />
+              <ControlledSelect<PriceTax>
+                label={t('tax')}
+                name="priceTaxId"
+                containerClass="mb-3"
+                className="form-select"
+                apiPath="prices/taxes"
+                option={{ label: row => row.name, value: row => row.id }}
+              />
+
+              <FormButtons />
+            </CardBody>
+          </Card>
+        </div>
+      </FormProvider>
+    </>
+  )
+}
