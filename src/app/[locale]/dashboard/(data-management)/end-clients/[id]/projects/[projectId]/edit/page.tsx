cb1027d17c462bf64ee48d22a6caba6a76d655be
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-12-03 14:30:25 +0700

    fix: Update the Address options of End Client Project create and update

--- src/app/[locale]/dashboard/(data-management)/end-clients/[id]/projects/[projectId]/edit/page.tsx
+++ src/app/[locale]/dashboard/(data-management)/end-clients/[id]/projects/[projectId]/edit/page.tsx
@@ -7,7 +7,7 @@ import { FormButtons } from '@/components/forms/form-buttons'
 import { FormProvider } from '@/components/forms/form-provider'
 import { ControlledSelect } from '@/components/forms/select'
 import { PageTitle } from '@/components/page-title'
-import { EndClientAddress } from '@/services/swr/models/end-client-address.type'
+import { Address } from '@/services/swr/models/address.type'
 import { OrganizationUnit } from '@/services/swr/models/organization-unit.type'
 import { Project } from '@/services/swr/models/project.type'
 
@@ -38,13 +38,13 @@ export default function UpdateEndClientProject({
                 apiPath={'projects'}
                 option={{ label: row => row.projectName, value: row => row.id }}
               />
-              <ControlledSelect<EndClientAddress>
+              <ControlledSelect<Address>
                 label={t('address')}
                 name="endclientAddressesId"
                 containerClass="mb-3"
                 className="form-control-solid"
-                apiPath={`end-clients/${params.id}/addresses`}
-                option={{ label: row => row.address.addressName, value: row => row.id }}
+                apiPath={`addresses`}
+                option={{ label: row => row.addressName, value: row => row.id }}
               />
               <ControlledSelect<OrganizationUnit>
                 label={t('organisationalUnit')}
