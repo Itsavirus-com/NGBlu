9905fcb28cd465fa359f20640d5090df18e188d4
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-12-03 14:31:19 +0700

    fix(enterprise-root-project-crud): Update the Address options of Enterprise Root Project create and update

--- src/app/[locale]/dashboard/(data-management)/enterprise-roots/[id]/projects/new/page.tsx
+++ src/app/[locale]/dashboard/(data-management)/enterprise-roots/[id]/projects/new/page.tsx
@@ -7,7 +7,7 @@ import { FormButtons } from '@/components/forms/form-buttons'
 import { FormProvider } from '@/components/forms/form-provider'
 import { ControlledSelect } from '@/components/forms/select'
 import { PageTitle } from '@/components/page-title'
-import { EnterpriseRootAddress } from '@/services/swr/models/enterprise-root-address.type'
+import { Address } from '@/services/swr/models/address.type'
 import { OrganizationUnit } from '@/services/swr/models/organization-unit.type'
 import { Project } from '@/services/swr/models/project.type'
 
@@ -34,13 +34,13 @@ export default function NewEnterpriseRootProject({ params }: { params: { id: num
                 apiPath={'projects'}
                 option={{ label: row => row.projectName, value: row => row.id }}
               />
-              <ControlledSelect<EnterpriseRootAddress>
+              <ControlledSelect<Address>
                 label={t('address')}
                 name="enterpriseRootAddressesId"
                 containerClass="mb-3"
                 className="form-control-solid"
-                apiPath={`enterprise-roots/${params.id}/addresses`}
-                option={{ label: row => row.address.addressName, value: row => row.id }}
+                apiPath={`addresses`}
+                option={{ label: row => row.addressName, value: row => row.id }}
               />
               <ControlledSelect<OrganizationUnit>
                 label={t('organisationalUnit')}
