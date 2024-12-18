ecfffc69889bd0bdc20f919a51e2d9ea4769ee04
Chlaudya Margareta <chlaudya@itsavirus.com>
2024-12-18 18:17:27 +0800

    fix: missing field end clients

--- src/app/[locale]/dashboard/(data-management)/end-clients/components/end-client-form.hook.ts
+++ src/app/[locale]/dashboard/(data-management)/end-clients/components/end-client-form.hook.ts
@@ -19,16 +19,46 @@ export default function useEndClientForm(id?: number) {
     typeId: yup.number().required(),
     statusId: yup.number().required(),
     locationAddressId: yup.number().required(),
+    contactPersonId: yup.number(),
+    accountNumber: yup.string().ensure().max(45),
+    referenceId: yup.string(),
+    personId: yup.number(),
+    afasId: yup.string().ensure().max(45),
+    companyInfoId: yup.number(),
   })
 
   const methods = useForm<InferType<typeof schema>>({
     resolver: yupResolver(schema),
-    values: endClient,
+    values: {
+      name: endClient?.name ?? '',
+      typeId: endClient?.typeId ?? 0,
+      statusId: endClient?.statusId ?? 0,
+      locationAddressId: endClient?.locationAddressId ?? 0,
+      contactPersonId: endClient?.contactPersonId ?? 0,
+      accountNumber: endClient?.accountNumber ?? '',
+      referenceId: endClient?.referenceId ?? '',
+      personId: endClient?.personId ?? 0,
+      afasId: endClient?.afasId ?? '',
+      companyInfoId: endClient?.companyInfoId ?? 0,
+    },
   })
 
   const addNewEndClient = async (data: InferType<typeof schema>) => {
+    const payload = {
+      name: data.name,
+      typeId: data.typeId,
+      statusId: data.statusId,
+      locationAddressId: data.locationAddressId,
+      contactPersonId: data.contactPersonId,
+      accountNumber: data.accountNumber,
+      referenceId: data.referenceId,
+      personId: data.personId,
+      afasId: data.afasId,
+      companyInfoId: data.companyInfoId,
+    }
+
     try {
-      const res = await endClientApi.new(data)
+      const res = await endClientApi.new(payload)
 
       if (res.ok) {
         showToast({ variant: 'success', body: 'End client created successfully' })
@@ -42,8 +72,21 @@ export default function useEndClientForm(id?: number) {
   const updateEndClient = async (data: InferType<typeof schema>) => {
     if (!id) return
 
+    const payload = {
+      name: data.name,
+      typeId: data.typeId,
+      statusId: data.statusId,
+      locationAddressId: data.locationAddressId,
+      contactPersonId: data.contactPersonId,
+      accountNumber: data.accountNumber,
+      referenceId: data.referenceId,
+      personId: data.personId,
+      afasId: data.afasId,
+      companyInfoId: data.companyInfoId,
+    }
+
     try {
-      const res = await endClientApi.update(id, data)
+      const res = await endClientApi.update(id, payload)
 
       if (res.ok) {
         showToast({ variant: 'success', body: 'End client updated successfully' })
