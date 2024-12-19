138566a7e25af5134b0f71ca4bb2f36864381b5e
Chlaudya Margareta <chlaudya@itsavirus.com>
2024-12-19 09:59:59 +0800

    fix: missing field company add origin type

--- src/app/[locale]/dashboard/(data-management)/companies/components/company-form.hook.ts
+++ src/app/[locale]/dashboard/(data-management)/companies/components/company-form.hook.ts
@@ -64,7 +64,7 @@ export default function useCompanyForm(companyId?: number) {
       legalAddressId: company?.legalAddressId ?? 0,
       chamberOfCommerceId: company?.chamberOfCommerceId ?? '',
       vatNumber: company?.vatNumber ?? '',
-      originId: 0,
+      originId: company?.origin.id ?? 0,
     },
   })
 
--- src/services/swr/models/company.type.ts
+++ src/services/swr/models/company.type.ts
@@ -1,10 +1,30 @@
 import { Address } from './address.type'
 import { CompanyStatus } from './company-status.type'
 
+export type Origin = {
+  id: number
+  originId: number
+  companyName: string
+  companyStatusId: number
+  companyStatus: string | null
+  vatNumber: string
+  chamberOfCommerceId: string
+  legalAddressId: number
+  legalAddress: Address
+  visitAddressId: number
+  visitAddress: Address
+  postalAddressId: number
+  postalAddress: Address
+  invoiceAddressId: number
+  invoiceAddress: Address
+  createdAt: string
+  updatedAt: string
+}
+
 export type Company = {
   id: number
   originId: number
-  origin: Record<string, unknown>
+  origin: Origin
   companyname: string
   vatNumber: string
   companyStatusId: number
