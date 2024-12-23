5eaa929ec92d4751234032efe3d72342842b74c3
Chlaudya Margareta <chlaudya@itsavirus.com>
2024-12-23 12:51:47 +0800

    feat: add breadcrumbs on enterprise-roots page

--- src/app/[locale]/dashboard/(data-management)/enterprise-roots/[id]/customers/[customerId]/page.tsx
+++ src/app/[locale]/dashboard/(data-management)/enterprise-roots/[id]/customers/[customerId]/page.tsx
@@ -3,6 +3,8 @@
 import { useTranslations } from 'next-intl'
 import { Row } from 'react-bootstrap'
 
+import { Breadcrumbs } from '@/components/breadcrumbs/breadcrumbs'
+import { getBreadcrumbItems } from '@/components/breadcrumbs/helper'
 import { Page } from '@/components/page/page'
 import { PageTitle } from '@/components/page-title'
 import { TextView } from '@/components/view/text-view/text-view'
@@ -19,6 +21,10 @@ export default function EnterpriseRootCustomerDetails({
 
   return (
     <>
+      <div className="app-container">
+        <Breadcrumbs items={getBreadcrumbItems(data)} />
+      </div>
+
       <PageTitle title={data?.endclient.name || ''} />
 
       <Page title={t('endClient')}>
