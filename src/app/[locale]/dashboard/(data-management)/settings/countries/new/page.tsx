662299972cfbf063444681022fe0b95b9202de1a
Chlaudya Margareta <chlaudya@itsavirus.com>
2025-01-13 14:38:45 +0800

    refactor: add required fields country

--- src/app/[locale]/dashboard/(data-management)/settings/countries/new/page.tsx
+++ src/app/[locale]/dashboard/(data-management)/settings/countries/new/page.tsx
@@ -41,18 +41,21 @@ export default function NewCountry() {
                 name="locale"
                 containerClass="mb-3"
                 className="form-control-solid"
+                isRequired
               />
               <ControlledInput
                 label={t('decimalSymbol')}
                 name="decimalSymbol"
                 containerClass="mb-3"
                 className="form-control-solid"
+                isRequired
               />
               <ControlledInput
                 label={t('iso')}
                 name="iso"
                 containerClass="mb-3"
                 className="form-control-solid"
+                isRequired
               />
 
               <FormButtons />
